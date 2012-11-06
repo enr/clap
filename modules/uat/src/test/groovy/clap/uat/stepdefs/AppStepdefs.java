f8045d591a358bc91cdb353a08548402ad837cda
enr <enrico.comiti@gmail.com>
2012-11-06 17:01:49 +0100

    update deps

--- modules/uat/src/test/groovy/clap/uat/stepdefs/AppStepdefs.java
+++ modules/uat/src/test/groovy/clap/uat/stepdefs/AppStepdefs.java
@@ -13,9 +13,9 @@ import com.github.enr.clap.util.ClasspathUtil;
 import com.google.common.base.Joiner;
 import com.google.inject.Module;
 
-import cucumber.annotation.en.Given;
-import cucumber.annotation.en.Then;
-import cucumber.annotation.en.When;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class AppStepdefs {
     
