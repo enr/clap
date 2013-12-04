34270649f024f797b27318ac8b29c7a22502d730
enr <enrico.comiti@gmail.com>
2013-12-04 10:21:00 +0100

    Remove reference to deprecated class

--- modules/uat/src/test/groovy/clap/uat/RunCukesTest.java
+++ modules/uat/src/test/groovy/clap/uat/RunCukesTest.java
@@ -1,9 +1,11 @@
 package clap.uat;
 
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
 @RunWith(Cucumber.class)
-@Cucumber.Options(features = "src/test/resources/features", format = {"html:build/cucumber-report", "json:build/cucumber-json-report.json"})
+@CucumberOptions(features = "src/test/resources/features", format = {"html:build/cucumber-report", "json:build/cucumber-json-report.json"})
 public class RunCukesTest {
 }
